081064a17140c938b774d4fc752189e386aa3579
henry <1151071+HenrYxZ@users.noreply.github.com>
2020-04-17 23:32:33 -0500

    tests: add test for triangle barycentric coordinates

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,2 @@
 from test_ray import RayTestCase
+from test_triangle import TriangleTestCase
--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,14 +1,39 @@
 import unittest
+import numpy as np
 # Local modules
 from object import Triangle
+from vertex import Vertex
+import shaders
+import utils
 
 
 class TriangleTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        self.v0 = Vertex(np.array([-1.0, 0.0, 0.0]))
+        self.v1 = Vertex(np.array([1.0, 0.0, 0.0]))
+        self.v2 = Vertex(np.array([0.0, 1.0, 0.0]))
+        self.triangle = Triangle(
+            utils.MTL_DIFFUSE_BLUE,
+            shaders.TYPE_DIFFUSE_COLORS,
+            self.v0,
+            self.v1,
+            self.v2
+        )
 
     def test_barycentric_coordinates(self):
-        pass
+        s, t = self.triangle.get_barycentric_coord(self.v0.position)
+        self.assertEqual(s, 0)
+        self.assertEqual(t, 0)
+        s, t = self.triangle.get_barycentric_coord(self.v1.position)
+        self.assertEqual(s, 1)
+        self.assertEqual(t, 0)
+        s, t = self.triangle.get_barycentric_coord(self.v2.position)
+        self.assertEqual(s, 0)
+        self.assertEqual(t, 1)
+        ph = self.v1.position * 0.7 + self.v2.position * 0.3
+        s, t = self.triangle.get_barycentric_coord(ph)
+        self.assertAlmostEqual(s, 0.7)
+        self.assertAlmostEqual(t, 0.3)
 
 
 if __name__ == '__main__':
