1ed697ab7f039381735e0fab6db1203b00bf3d43
henry <1151071+HenrYxZ@users.noreply.github.com>
2020-10-08 15:30:39 -0500

    feat: add timer to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import time
+
 # Local imports
 from material import Material
 import material
@@ -35,12 +37,29 @@ def humanize_time(secs):
 def degree2radians(degrees):
     return (degrees / float(360)) * 2 * np.pi
 
+
 def random_unit_vector():
     # 3D random vector
     v = np.random.random_sample(3)
     v_unit = normalize(v)
     return v_unit
 
+
+class Timer:
+    def __init__(self):
+        self.start_time = 0
+        self.end_time = 0
+        self.elapsed_time = 0
+
+    def start(self):
+        self.start_time = time.time()
+
+    def stop(self):
+        self.end_time = time.time()
+        self.elapsed_time = self.end_time - self.start_time
+
+    def __str__(self):
+        return humanize_time(self.elapsed_time)
 
 # class Position(object):
 #     """Position in 3D space"""
